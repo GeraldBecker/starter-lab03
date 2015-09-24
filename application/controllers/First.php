0d0b719996460aac23b60141257dd2a931592cd9
Gerald Becker <ger_0_1@hotmail.com>
2015-09-24 15:34:01 -0700

    Fixed the link sleep by remapping it to the First controller with a method zzz. The method calls the quote #1

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -29,7 +29,19 @@ class First extends Application {
 
         $this->render();
     }
+    
+    function zzz() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // grab the content for this person
+        $source = $this->quotes->get('1');
+        
+        
+        $this->data['mug'] = $source['mug'];
+        $this->data['who'] = $source['who'];        
+        $this->data['what'] = $source['what'];
 
+        $this->render();
+    }
 }
 
 /* End of file Welcome.php */
