29139a6f31b317ce8b50a7aad6b96ad55ef3f885
Gerald Becker <ger_0_1@hotmail.com>
2015-09-24 15:59:25 -0700

    Fixed the link for lock/em/up and made it so the last later parameters are wildcards. Pulls the 2nd person.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -30,7 +30,19 @@ class Welcome extends Application {
 
         $this->render();
     }
+    
+    function shucks() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // grab the content for this person
+        $source = $this->quotes->get('2');
+        
+        
+        $this->data['mug'] = $source['mug'];
+        $this->data['who'] = $source['who'];        
+        $this->data['what'] = $source['what'];
 
+        $this->render();
+    }
 }
 
 /* End of file Welcome.php */
