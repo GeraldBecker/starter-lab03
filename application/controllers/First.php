fa157f8d910e40c419f2e4312808080b1705016e
Gerald Becker <ger_0_1@hotmail.com>
2015-09-24 16:20:57 -0700

    Fixed the link show/3. It is set up to take a parameter and pass it into the function. This would be the quote number that is loaded however 3 is currently hard-coded. Switch to $num if the quote loaded should reflect the param.

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -42,6 +42,19 @@ class First extends Application {
 
         $this->render();
     }
+    
+    function gimme($num) {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // grab the content for this person
+        $source = $this->quotes->get('3'); //Use $num to use the parameter to get that person
+        
+        
+        $this->data['mug'] = $source['mug'];
+        $this->data['who'] = $source['who'];        
+        $this->data['what'] = $source['what'];
+
+        $this->render();
+    }
 }
 
 /* End of file Welcome.php */
